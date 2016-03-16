635a1f725a7dc9d5ee3d21a852c30f4b91d0115c
Remsey <remsey.diy@gmail.com>
2016-03-16 15:49:29 +0100

    wersja 1.0

--- R1.sh
+++ R1.sh
@@ -4,3 +4,22 @@ ip addr add 10.0.1.1/24 dev eth0
 ip addr add 10.0.200.1/24 dev eth1
 ip addr add 192.168.0.1/24 dev eth2
 echo 1 > /proc/sys/net/ipv4/ip_forward
+echo "#### DONE ####"
+# /etc/quagga/zebra.conf
+echo "hostname click" > /etc/quagga/zebra.conf
+echo "password toor" >> /etc/quagga/zebra.conf
+echo "enable password toor" >> /et/quagga/zebra.conf
+# /etc/quagga/ripd.conf
+echo "! Script by krzysiek" > /etc/quagga/ripd.conf
+echo "hostname click" >> /etc/quagga/ripd.conf
+echo "password toor" >> /etc/quagga/ripd.conf
+echo "enable password toor" >> /etc/quagga/ripd.conf
+echo "!" >> /etc/quagga/ripd.conf
+echo "router rip" >> /etc/quagga/ripd.conf
+echo " reditribute connected" >> /etc/quagga/ripd.conf
+echo " network 10.0.1.0/24" >> /etc/quagga/ripd.conf
+echo " network 10.0.200.0/24" >> /etc/quagga/ripd.conf
+echo "!" >> /etc/quagga/ripd.conf
+echo "line vty" >> /etc/quagga/ripd.conf
+echo "!" >> /etc/quagga/ripd.conf
+echo "#### DONE ####"
