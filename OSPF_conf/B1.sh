950da5588245667f296f870c7c91f4e2d28b91a5
Remsey <remsey.diy@gmail.com>
2016-03-22 15:27:24 +0100

    wersja 2.0

---
+++ OSPF_conf/B1.sh
@@ -0,0 +1,2 @@
+ip addr add 172.16.0.2/24 dev eth0
+ip route add default via 172.16.0.1
---
+++ OSPF_conf/R1.sh
@@ -0,0 +1,28 @@
+ip link set dev eth1 up
+ip link set dev eth2 up
+ip addr add 10.0.1.1/24 dev eth0
+ip addr add 10.0.200.1/24 dev eth1
+ip addr add 192.168.0.1/24 dev eth2
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sleep 2
+echo "#### DONE ####"
+# /etc/quagga/zebra.conf
+echo "hostname click" > /etc/quagga/zebra.conf
+echo "password toor" >> /etc/quagga/zebra.conf
+echo "enable password toor" >> /et/quagga/zebra.conf
+# /etc/quagga/ospfd.conf
+echo "! from github.com/Remsey/TS2Lab" > /etc/quagga/ospfd.conf
+echo "hostname click" >> /etc/quagga/ospfd.conf
+echo "password toor" >> /etc/quagga/ospfd.conf
+echo "enable password toor" >> /etc/quagga/ospfd.conf
+echo "!" >> /etc/quagga/ospfd.conf
+echo "router ospf" >> /etc/quagga/ospfd.conf
+echo " redistribute connected" >> /etc/quagga/ospfd.conf
+echo " network 192.168.0.0/24 area 0.0.0.0" >> /etc/quagga/ospfd.conf
+echo " network 10.0.1.0/24 area 0.0.0.0" >> /etc/quagga/ospfd.conf
+echo " network 10.0.200.0/24 area 0.0.0.0" >> /etc/quagga/ospfd.conf
+echo "!" >> /etc/quagga/ospfd.conf
+echo "line vty" >> /etc/quagga/ospfd.conf
+echo "!" >> /etc/quagga/ospfd.conf
+sleep 1
+echo "#### DONE ####"
